e9786cf1f0b8dd0a56bca1265509c72e9ea07eb0
It25101788 <It25101788@my.sliit.lk>
2026-09-14 23:28:38 +0530

    chore: initialize Spring Boot project scaffolding

---
+++ src/main/java/com/boatsafari/backend/BoatSafariBackendApplication.java
@@ -0,0 +1,13 @@
+package com.boatsafari.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoatSafariBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoatSafariBackendApplication.class, args);
+	}
+
+}
