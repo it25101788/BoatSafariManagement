7074d2c5adea204d1a20c53269629bc94b4e8009
It25101788 <It25101788@my.sliit.lk>
2026-09-15 02:41:44 +0530

    chore: add CORS configuration and application context test

---
+++ src/test/java/com/boatsafari/backend/BoatSafariBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boatsafari.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoatSafariBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
